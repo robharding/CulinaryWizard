74e4b0ffe27f6ac265f4026dc12d4e4820b03eb0
Rob Harding <robbie.harding@hotmail.com>
2023-09-19 19:55:35 -0400

    Hero text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Balancer from "react-wrap-balancer";
+
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-8 mt-24">
-      <h1>Hello, World!</h1>
+    <main className="flex flex-col items-center gap-8">
+      <div className="m-12"></div>
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold text-center">
+        <Balancer className="mx-5">
+          Elevate Your Culinary Creations
+          <br /> Without the <span className="text-primary">Life Stories</span>
+        </Balancer>
+      </h1>
     </main>
   );
 }
