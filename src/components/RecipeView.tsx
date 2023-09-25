f919ebcb84c4b8dedd9b2bb9f6564d1946e05cd8
Rob Harding <robbie.harding@hotmail.com>
2023-09-25 16:42:06 -0400

    Simple ingredients list and method

--- src/components/RecipeView.tsx
+++ src/components/RecipeView.tsx
@@ -1,6 +1,7 @@
 import { Recipe } from "@/lib/db/schema/recipe";
 import { FC } from "react";
 import Tags from "./Tags";
+import { Clock } from "lucide-react";
 
 interface RecipeViewProps {
   recipe: Recipe;
@@ -8,11 +9,36 @@ interface RecipeViewProps {
 
 const RecipeView: FC<RecipeViewProps> = ({ recipe }) => {
   return (
-    <div className="mt-8">
+    <div className="my-8">
       <h1 className="text-3xl font-bold">{recipe.title}</h1>
       <h2 className="text-xl">{recipe.description}</h2>
 
       <Tags tags={recipe.tags} />
+
+      <div className="flex flex-row items-center mt-2">
+        <Clock className="w-3 h-3 mr-2" />
+        <span className="">{recipe.cookTime}</span>
+      </div>
+
+      <div className="m-4" />
+
+      <h3 className="font-bold text-lg">Ingredients</h3>
+      {recipe.ingredients.map((ingredient, i) => (
+        <div key={i}>
+          <input type="checkbox" className="mr-2" />
+          <span>{ingredient}</span>
+        </div>
+      ))}
+
+      <div className="m-4" />
+
+      <h3 className="font-bold text-lg">Method</h3>
+      {recipe.method.map((instruction, i) => (
+        <div key={i}>
+          <input type="checkbox" className="mr-2" />
+          <span>{instruction}</span>
+        </div>
+      ))}
     </div>
   );
 };
