5dc658d415a3548b1e0975335fac9745956e7f6b
Rob Harding <robbie.harding@hotmail.com>
2023-09-29 14:17:38 -0400

    Remove computer router

--- src/lib/server/routers/_app.ts
+++ src/lib/server/routers/_app.ts
@@ -1,9 +1,7 @@
-import { computersRouter } from "./computers";
 import { router } from "../trpc";
 import { recipesRouter } from "./recipes";
 
 export const appRouter = router({
-  computers: computersRouter,
   recipes: recipesRouter,
 });
 
