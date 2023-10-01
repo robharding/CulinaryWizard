53d89fd71d593bb64897941617182be828cddbc5
Rob Harding <robbie.harding@hotmail.com>
2023-10-01 11:52:53 -0400

    unused imports

--- src/app/(withHero)/collection/page.tsx
+++ src/app/(withHero)/collection/page.tsx
@@ -1,16 +1,14 @@
 import RecipeCollection from "@/components/RecipeCollection";
 import { getUserAuth } from "@/lib/auth/utils";
 import type { NextPage } from "next";
-import { getServerSession } from "next-auth";
-import { RedirectType, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 interface CollectionPageProps {}
 
 const CollectionPage: NextPage<CollectionPageProps> = async ({}) => {
   const { session } = await getUserAuth();
   if (!session?.user) {
-    console.log("redirecting");
-    redirect("/sign-in", RedirectType.push);
+    redirect("/sign-in");
   }
 
   return (
