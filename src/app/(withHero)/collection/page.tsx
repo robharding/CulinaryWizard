7237a4794e8bb42e4327fe89a706a458f3fb78c9
Rob Harding <robbie.harding@hotmail.com>
2023-10-01 11:52:34 -0400

    Redirect logged out user from collection

--- src/app/(withHero)/collection/page.tsx
+++ src/app/(withHero)/collection/page.tsx
@@ -1,12 +1,18 @@
 import RecipeCollection from "@/components/RecipeCollection";
 import { getUserAuth } from "@/lib/auth/utils";
 import type { NextPage } from "next";
-import { signIn, useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { RedirectType, redirect } from "next/navigation";
 
 interface CollectionPageProps {}
 
 const CollectionPage: NextPage<CollectionPageProps> = async ({}) => {
+  const { session } = await getUserAuth();
+  if (!session?.user) {
+    console.log("redirecting");
+    redirect("/sign-in", RedirectType.push);
+  }
+
   return (
     <>
       <RecipeCollection />
